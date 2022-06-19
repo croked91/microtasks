a4291df554c20e64f09053bfc9615e5520ea82c4
Croked1991 <atik.egor@gmail.com>
2022-06-19 09:42:07 +0300

    custom select is done

--- src/components/Select/SelectCustom.tsx
+++ src/components/Select/SelectCustom.tsx
@@ -11,23 +11,48 @@ export const SelectCustom = () => {
 
     const [age, setAge] = useState("")
     const [x, setX] = useState(false)
+    const [z, setZ] = useState(false)
 
 
-    let i = localState.map(el => <li className={s.lishka}>{el.name}</li>)
-
-    let onOfListHandler = () => {
-        setX(!x)
+    let setTitleHandler = (el: string) => {
+        if (el === "None") {
+            setAge("")
+            setX(!x)
+            setZ(true)
+        } else {
+            setAge(el)
+            setX(!x)
+            setZ(true)
+        }
     }
 
+
+    let i = localState.map(el =>
+        <li onClick={() => setTitleHandler(el.name)}
+            className={el.name === age ? s.activeLishka + " " + s.lishka : s.lishka}>
+            {el.name}
+        </li>)
+
+    let onOffListHandler = () => {
+        setX(!x)
+        setZ(true)
+    }
+
+    let onBlurHandler = () => {
+        setZ(false)
+        setX(false)
+    }
+   
     return (
-        <div className={s.rootRoot}>
-            <div onClick={onOfListHandler} className={s.root}>
-                <label className={s.labelSelect}>Age</label>
-                <svg className={s.svg}><path className={s.path} d="M7 10l5 5 5-5z"></path></svg>
+        <div tabIndex={0} onBlur={onBlurHandler} className={s.rootRoot}>
+            <div onClick={onOffListHandler} className={z ? s.root + " " + s.rootNone + " " + s.borderBottomOn : s.root}>
+                <label className={z ? s.labelTop : s.labelSelect}>Age</label>
+                <label className={z ? s.labelListShow : s.labelListHidden}>{age}</label>
+                <svg className={x ? s.svgOpen : s.svg}><path className={s.path} d="M7 10l5 5 5-5z"></path></svg>
             </div>
             <div className={x ? s.rootList : s.rootlistOff}>
                 <ul className={s.list}>
-                    <li className={s.lishka + " " + s.none}>None</li>
+                    <li onClick={() => setTitleHandler("None")} className={s.lishka + " " + s.none}>None</li>
                     {i}
                 </ul>
             </div>
